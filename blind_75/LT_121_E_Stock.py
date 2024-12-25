31d54ae1cce4ba7cc667bff27115e81660790d72
Anna02malai <annamala@udel.edu>
2024-12-25 16:01:40 -0500

    Adding files

---
+++ blind_75/LT_121_E_Stock.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        local_min = prices[0]
+        global_max = 0
+        
+        for i in prices:
+            if i < local_min :
+                local_min = i
+            
+
+
+
+        # l,r = 0, 1 #left = Buying, right = Selling
+        # max_pft = 0
+
+        # while r < len(prices):
+        #     if prices[l] < prices[r] :
+        #         pft = prices[r] - prices[l]
+        #         max_pft = max(max_pft,pft)
+        #     else:
+        #         l = r
+            
+        #     r = r+1
+
+        # return max_pft
+    
+def main():
+    ans = Solution()
+    max_pft = ans.maxProfit([7, 1, 4, 2, 3, 6, 5])
+    print(max_pft)
+
+if __name__ == "__main__":
+    main()
+    
